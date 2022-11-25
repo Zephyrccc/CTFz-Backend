16876ccec10148227c6d85da6808d3528e387885
root <root@localhost.localdomain>
2022-11-26 00:17:29 +0800

    update

--- user/permissions.py
+++ user/permissions.py
@@ -2,9 +2,5 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return True
-
     def has_object_permission(self, request, view, obj):
         return obj.pk == request.user.pk
